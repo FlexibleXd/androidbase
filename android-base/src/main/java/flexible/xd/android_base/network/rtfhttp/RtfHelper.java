31712bbb746b3f739eef1126fb3d8b49e9d7ec9c
lgd <lgd123456>
2019-01-16 16:03:56 +0800

    添加过滤器

--- android-base/src/main/java/flexible/xd/android_base/network/rtfhttp/RtfHelper.java
+++ android-base/src/main/java/flexible/xd/android_base/network/rtfhttp/RtfHelper.java
@@ -4,11 +4,12 @@ import android.util.Log;
 
 import com.valdio.cookiejar.serializablecookiejar.SerializableCookieJar;
 
-import java.util.IdentityHashMap;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import flexible.xd.android_base.base.BaseApp;
 import flexible.xd.android_base.network.rtfhttp.conver.FastJsonConverterFactory;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -27,6 +28,7 @@ public class RtfHelper {
     private int rtfReadTimeout = 15;
     private int rtfWriteTimeout = 15;
     private Retrofit retrofit;
+    private OkHttpClient.Builder okHttpClient;
 
     private RtfHelper() {
     }
@@ -50,13 +52,12 @@ public class RtfHelper {
     public void init(String baseUrl) {
         rtfBaseUrl = baseUrl;
         SerializableCookieJar cookieJar = new SerializableCookieJar(BaseApp.getAppContext());
-        OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder()
+        okHttpClient = new OkHttpClient.Builder()
                 .hostnameVerifier((s, sslSession) -> true)
                 .cookieJar(cookieJar)
                 .connectTimeout(rtfConnectTimeout, TimeUnit.SECONDS)
                 .readTimeout(rtfReadTimeout, TimeUnit.SECONDS)
                 .writeTimeout(rtfWriteTimeout, TimeUnit.SECONDS);
-
         if (DEBUG) {
             //NONE：没有记录
             //BASIC：日志请求类型，URL，请求体的大小，响应状态和响应体的大小
@@ -66,10 +67,11 @@ public class RtfHelper {
                     message -> Log.i("flexible", message)
             );
             loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            httpBuilder.addInterceptor(loggingInterceptor);
+            okHttpClient.addInterceptor(loggingInterceptor);
         }
 
-        OkHttpClient okHttpClient = httpBuilder.build();
+        OkHttpClient okHttpClient = this.okHttpClient.build();
+
 
         retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
@@ -77,6 +79,46 @@ public class RtfHelper {
                 .addConverterFactory(FastJsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
+
+    }
+
+    /**
+     * 初始化
+     *
+     * @param baseUrl
+     */
+    public void init(String baseUrl, Interceptor interceptor) {
+        rtfBaseUrl = baseUrl;
+        SerializableCookieJar cookieJar = new SerializableCookieJar(BaseApp.getAppContext());
+        okHttpClient = new OkHttpClient.Builder()
+                .hostnameVerifier((s, sslSession) -> true)
+                .cookieJar(cookieJar)
+                .connectTimeout(rtfConnectTimeout, TimeUnit.SECONDS)
+                .readTimeout(rtfReadTimeout, TimeUnit.SECONDS)
+                .writeTimeout(rtfWriteTimeout, TimeUnit.SECONDS);
+        okHttpClient.addInterceptor(interceptor);
+        if (DEBUG) {
+            //NONE：没有记录
+            //BASIC：日志请求类型，URL，请求体的大小，响应状态和响应体的大小
+            //HEADERS：日志请求和响应头，请求类型，URL，响应状态
+            //BODY：日志请求和响应标头和正文
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(
+                    message -> Log.i("flexible", message)
+            );
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            okHttpClient.addInterceptor(loggingInterceptor);
+        }
+
+        OkHttpClient okHttpClient = this.okHttpClient.build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .addConverterFactory(FastJsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+
     }
 
     /**
@@ -86,7 +128,7 @@ public class RtfHelper {
      */
     public void init(String baseUrl, boolean debug) {
         this.DEBUG = debug;
-        init(baseUrl);
+        init(baseUrl, null);
     }
 
     /**
@@ -110,5 +152,13 @@ public class RtfHelper {
             throw new RuntimeException("you should init first ");
         }
         return (T) retrofit.create(t);
+    }
+
+
+    public OkHttpClient.Builder getOkHttpClient() {
+        if (rtfBaseUrl == null) {
+            throw new RuntimeException("you should init first ");
+        }
+        return okHttpClient;
     }
 }
