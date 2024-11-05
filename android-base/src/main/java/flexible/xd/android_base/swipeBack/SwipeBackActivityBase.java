b5def9a7304472868d3fe73bd56c770f32eb1f77
flexible <lgd19940421@163.com>
2024-11-05 21:01:51 +0800

    *

--- android-base/src/main/java/flexible/xd/android_base/swipeBack/SwipeBackActivityBase.java
+++ android-base/src/main/java/flexible/xd/android_base/swipeBack/SwipeBackActivityBase.java
@@ -9,12 +9,12 @@ public  interface SwipeBackActivityBase {
     /**
      * @return the SwipeBackLayout associated with this activity.
      */
-    public abstract SwipeBackLayout getSwipeBackLayout();
+     abstract SwipeBackLayout getSwipeBackLayout();
 
-    public abstract void setSwipeBackEnable(boolean enable);
+     abstract void setSwipeBackEnable(boolean enable);
 
     /**
      * Scroll out contentView and finish the activity
      */
-    public abstract void scrollToFinishActivity();
+     abstract void scrollToFinishActivity();
 }
