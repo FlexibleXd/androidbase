0b903fe1f5a484ca09c5e26247868b2b982a3340
flexible <lgd19940421@163.com>
2024-11-05 15:35:38 +0800

    修改网络框架

---
+++ android-base/src/test/java/flexible/xd/android_base/Body.java
@@ -0,0 +1,13 @@
+package flexible.xd.android_base;
+
+public class Body {
+    Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
---
+++ android-base/src/test/java/flexible/xd/android_base/User.java
@@ -0,0 +1,22 @@
+package flexible.xd.android_base;
+
+public class User {
+    String name;
+    String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
